07361c44c78ea6573071017c068524dad35880a7
！ʞ◕‿◕ʞ ∀ <116001127+Yourbihari@users.noreply.github.com>
2025-08-10 02:07:36 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -9,7 +9,14 @@ export default function Home() {
 
     useEffect(() => {
       const typed = new Typed(el.current, {
-        strings: ['Coding', 'Web Development', 'Software Engineering', 'Data Science', 'Machine Learning'],
+        strings: ['Ethical Hackers',
+        'Black Hat Hackers',
+        'Gray Hat Hackers',
+        'Suicide Hackers',
+        'Script Kiddies',
+        'State-Sponsored',
+        'Hacktivist',
+        'Cyber Terrorists'],
         typeSpeed: 50,
       });
   
@@ -24,10 +31,10 @@ export default function Home() {
       <section className="container px-4 py-10 mx-auto lg:h-128 lg:space-x-8 lg:flex lg:items-center">
         <div className="w-full text-center lg:text-left lg:w-1/2 lg:-mt-8">
           <h1 className="text-3xl leading-snug text-gray-800 dark:text-gray-200 md:text-4xl">
-            A <span className="font-semibold">free repository</span> for community <br className="hidden lg:block" /> components using <span className="font-semibold underline decoration-primary"><span ref={el} /></span>
+            A <span className="font-semibold">free repository</span> for Hacking <br className="hidden lg:block" /> Enthusiasts <span className="font-semibold underline decoration-primary"><span ref={el} /></span>
           </h1>
           <p className="mt-4 text-lg text-gray-500 dark:text-gray-300">
-            Open source Tailwind UI components and templates to <br className="hidden lg:block" /> bootstrap your new apps, projects or landing sites!
+            Open Source Hacking Tools, Scripts, and  <br className="hidden lg:block" /> UI Components to Boost Your Next Security <br className="hidden lg:block" />Project or CTF Challenge!
           </p>
           <div className="mt-6 bg-transparent border rounded-lg dark:border-gray-700 lg:w-2/3 focus-within:border-primary focus-within:ring focus-within:ring-primary dark:focus-within:border-primary focus-within:ring-opacity-20">
             <form action="https://www.creative-tim.com/twcomponents/search" className="flex flex-wrap justify-between md:flex-row">
@@ -109,7 +116,7 @@ export default function Home() {
       {/* Testimonial 1 */}
       <div className="w-full sm:w-1/2 lg:w-1/3 p-4">
         <div className="p-6 bg-white rounded-lg shadow-lg dark:bg-gray-800 transform transition duration-500 hover:scale-105 text-center">
-          <p className="text-gray-600 dark:text-gray-400">"This service has been a game-changer for our business. Highly recommend!"</p>
+          <p className="text-gray-600 dark:text-gray-400">&quot;This service has been a game-changer for our business. Highly recommend!&quot;</p>
           <h3 className="mt-4 text-xl font-semibold text-gray-800 dark:text-gray-200">John Doe</h3>
           <p className="text-gray-500 dark:text-gray-300">CEO, Company A</p>
         </div>
@@ -117,7 +124,7 @@ export default function Home() {
       {/* Testimonial 2 */}
       <div className="w-full sm:w-1/2 lg:w-1/3 p-4">
         <div className="p-6 bg-white rounded-lg shadow-lg dark:bg-gray-800 transform transition duration-500 hover:scale-105 text-center">
-          <p className="text-gray-600 dark:text-gray-400">"Amazing experience! The team was professional and the results were outstanding."</p>
+          <p className="text-gray-600 dark:text-gray-400">&quot;Amazing experience! The team was professional and the results were outstanding.&quot;</p>
           <h3 className="mt-4 text-xl font-semibold text-gray-800 dark:text-gray-200">Jane Smith</h3>
           <p className="text-gray-500 dark:text-gray-300">Marketing Director, Company B</p>
         </div>
@@ -125,7 +132,7 @@ export default function Home() {
       {/* Testimonial 3 */}
       <div className="w-full sm:w-1/2 lg:w-1/3 p-4">
         <div className="p-6 bg-white rounded-lg shadow-lg dark:bg-gray-800 transform transition duration-500 hover:scale-105 text-center">
-          <p className="text-gray-600 dark:text-gray-400">"Exceptional service and support. We couldn't be happier with the results."</p>
+          <p className="text-gray-600 dark:text-gray-400">&quot;Exceptional service and support. We couldn&apos;t be happier with the results.&quot;</p>
           <h3 className="mt-4 text-xl font-semibold text-gray-800 dark:text-gray-200">Michael Brown</h3>
           <p className="text-gray-500 dark:text-gray-300">CTO, Company C</p>
         </div>
@@ -147,7 +154,7 @@ export default function Home() {
         <div className="p-6 bg-white rounded-lg shadow-lg dark:bg-gray-800 transform transition duration-500 hover:scale-105">
           <img src="/typescript.webp" className="w-full h-64 object-cover rounded-t-lg"/>
           <div className="mt-4">
-            <h3 className="text-xl font-semibold text-gray-800 dark:text-gray-200">Blog Post Title 1</h3>
+            <h3 className="text-xl font-semibold text-gray-800 dark:text-gray-200">White Hat Hacker</h3>
             <p className="mt-2 text-gray-600 dark:text-gray-400">A brief description of the blog post goes here. It should be engaging and informative.</p>
             <Button className="m-2" variant="outline" href="/blog-post-1">Read More</Button>
           </div>
@@ -158,7 +165,7 @@ export default function Home() {
         <div className="p-6 bg-white rounded-lg shadow-lg dark:bg-gray-800 transform transition duration-500 hover:scale-105">
           <img src="https://images.pexels.com/photos/1181472/pexels-photo-1181472.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="Blog 2" className="w-full h-64 object-cover rounded-t-lg"/>
           <div className="mt-4">
-            <h3 className="text-xl font-semibold text-gray-800 dark:text-gray-200">Blog Post Title 2</h3>
+            <h3 className="text-xl font-semibold text-gray-800 dark:text-gray-200">Black Hat Hacker</h3>
             <p className="mt-2 text-gray-600 dark:text-gray-400">A brief description of the blog post goes here. It should be engaging and informative.</p>
             <Button className="m-2" variant="outline" href="/blog-post-2">Read More</Button>
           </div>
@@ -169,7 +176,7 @@ export default function Home() {
         <div className="p-6 bg-white rounded-lg shadow-lg dark:bg-gray-800 transform transition duration-500 hover:scale-105">
           <img src="https://images.pexels.com/photos/3861972/pexels-photo-3861972.jpeg" alt="Blog 3" className="w-full h-64 object-cover rounded-t-lg"/>
           <div className="mt-4">
-            <h3 className="text-xl font-semibold text-gray-800 dark:text-gray-200">Blog Post Title 3</h3>
+            <h3 className="text-xl font-semibold text-gray-800 dark:text-gray-200">Gray Hat Hacker</h3>
             <p className="mt-2 text-gray-600 dark:text-gray-400">A brief description of the blog post goes here. It should be engaging and informative.</p>
             <Button className="m-2" variant="outline" href="/blog-post-3">Read More</Button>
           </div>
@@ -182,11 +189,3 @@ export default function Home() {
     </main>
   );
 };
-
-
-
-
-
-
-
-
